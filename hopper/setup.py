c6b466a4fa2ef95769a9b2f8dcb50a33f9f19fb1
Jackmin801 <ongjackm@gmail.com>
2025-08-11 09:17:28 -0700

    modify paths and builds to have torch 2.8

--- hopper/setup.py
+++ hopper/setup.py
@@ -35,7 +35,7 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 
 PACKAGE_NAME = "flash_attn_3"
 
-BASE_WHEEL_URL = "https://github.com/Dao-AILab/flash-attention/releases/download/{tag_name}/{wheel_name}"
+BASE_WHEEL_URL = "https://github.com/Jackmin801/flash-attention/releases/download/{tag_name}/{wheel_name}"
 
 # FORCE_BUILD: Force a fresh build locally, instead of attempting to find prebuilt wheels
 # SKIP_CUDA_BUILD: Intended to allow CI to use a simple `python setup.py sdist` run to copy over raw files, without any cuda compilation
